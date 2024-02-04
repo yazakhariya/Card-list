e2de07e2022696f6373d95fbb46dbaeed368d4e6
yazakhariya <17070501.iana@gmail.com>
2024-02-04 18:02:55 +0300

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Card-list",
   resolve: {
     alias: {
       src: '/src',
